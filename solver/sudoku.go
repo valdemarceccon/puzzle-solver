dd3ef754cc4af12c2e26fdd5d210823892cb61e0
Valdemar Ceccon <valdemar.ceccon@objective.com.br>
2020-01-10 18:00:23 -0300

    parsing done

--- solver/sudoku.go
+++ solver/sudoku.go
@@ -17,6 +17,12 @@ func NewSudoku(values string) (result *Sudoku, err error) {
 		return nil, errors.New("Cannot parse string")
 	}
 
+	result.parse(cleanedString)
+
+	if !result.IsValid() {
+		return nil, errors.New("puzzle is not in a valid state")
+	}
+
 	return
 }
 
@@ -33,16 +39,79 @@ func getAllNumbers(values string) string {
 	return cleanedString
 }
 
+func (s *Sudoku) parse(values string) {
+	for row := 0; row < 9; row ++ {
+		for col := 0; col < 9; col ++ {
+			n, _ := strconv.Atoi(string(values[row*col + col]))
+			s.tab[row][col] = uint8(n)
+		}
+	}
+}
+
 func (s *Sudoku) String() string {
 	result := ""
 
 	for _, lin := range s.tab {
 		for _, col := range lin {
 			result = result + strconv.Itoa(int(col)) + " "
-
 		}
 
 		result += "\n"
+	}
+
+	return result
+}
+
+func (s *Sudoku) IsValid() bool {
+	for i:= 0; i < 9; i++ {
+		if !isLineValid(i) || !isColumnValid(i) || !isQuadrantValid(i) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isQuadrantValid(i int) bool {
+
+	return true
+}
+
+func isColumnValid(i int) bool {
+
+	return true
+}
+
+func isLineValid(i int) bool {
+
+	return true
+}
+
+func (s *Sudoku) getColumn(col int) []uint8 {
+	result := make([]uint8, 9)
+
+	for linha := 0; linha < 9; linha++ {
+		result[linha] = s.tab[linha][col]
+	}
+
+	return result
+}
+
+func (s *Sudoku) getRow(row int) []uint8 {
+	result := make([]uint8, 9)
+
+	for col := 0; col < 9; col++ {
+		result[col] = s.tab[row][col]
+	}
+
+	return result
+}
+
+func (s *Sudoku) getQuadrant(q int) []uint8 {
+	result := make([]uint8, 9)
+
+	for quad := 0; quad < 9; quad++ {
+		result[quad] = s.tab[q][quad]
 	}
 
 	return result
--- solver/sudoku_test.go
+++ solver/sudoku_test.go
@@ -5,38 +5,46 @@ import (
 	"testing"
 )
 
-func TestSudoku(t *testing.T) {
-	t.Run("should parse correct string", func(t *testing.T) {
-		given := "123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789"
+func TestNewSudoku(t *testing.T) {
+	givenSolved := "957613284" +
+		"483257196" +
+		"612849537" +
+		"178364952" +
+		"524971368" +
+		"369528741" +
+		"845792613" +
+		"291436875" +
+		"736185429"
 
-		_, err := solver.NewSudoku(given)
-		if err != nil {
-			t.Fatal("should not have returned an error")
+	givenUnsolved := "000000200" +
+		"080007090" +
+		"602000500" +
+		"070060000" +
+		"000901000" +
+		"000020040" +
+		"005000603" +
+		"090400070" +
+		"006000000"
+
+	_ = givenUnsolved
+
+	t.Run("should parse correct string", func(t *testing.T) {
+		sudoku, err := solver.NewSudoku(givenSolved)
+		if err != nil || sudoku == nil {
+			t.Fatal("should not have returned an error, and the parse should have be done")
 		}
 	})
 
 	t.Run("should not parse incorrect string", func(t *testing.T) {
-		given := "123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789" +
-			"123456789111"
+		given := givenSolved + "1135456454"
 
 		_, err := solver.NewSudoku(given)
 		if err == nil {
 			t.Fatal("should have returned an error")
 		}
 	})
+}
+
+func TestSolveSudoku(t *testing.T) {
+
 }
